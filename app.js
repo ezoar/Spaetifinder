f3e71d49e20fd7c66b17d5df3dab806542488950
ezoar <noquierotenermailperolotengo@gmail.com>
2019-01-27 22:19:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Token and Fixing Center-Point of Map 
 
   
-  // Funktionierendes Beipsiel 1
+  // Token
 
   mapboxgl.accessToken = 'pk.eyJ1IjoiZW8tbWFwYm94IiwiYSI6ImNqcjEwYWN4ZzAzNDgzeXJzdGtlbGduNGgifQ.TBbP0PVssrORXh4sFvoOMA'; // replace this with your access token
   var map = new mapboxgl.Map({
@@ -11,27 +11,11 @@
     zoom: 10.7
   });
 
-  
-
-  /*
-  // Funktionierendes Beipsiel 2
-
-  mapboxgl.accessToken = 'pk.eyJ1IjoiZW8tbWFwYm94IiwiYSI6ImNqcjEwYWN4ZzAzNDgzeXJzdGtlbGduNGgifQ.TBbP0PVssrORXh4sFvoOMA';
-  const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/eo-mapbox/cjrfeenoz33wp2so4s7yer4b7',
-  center: [12.397613, 51.315664],
-  zoom: 10.7
-  });
-  */
-
-
-
 // Markers and Popups
 
   map.on('click', function(e) {
   var features = map.queryRenderedFeatures(e.point, {
-      layers: ['leipzig-spaetis'] // replace this with the name of the layer
+      layers: ['leipzig-spaetis'] // Name of the Layer
   });
 
   if (!features.length) {
@@ -42,9 +26,6 @@
 
   var popup = new mapboxgl.Popup({ offset: [0, -15] })
       .setLngLat(feature.geometry.coordinates)
-      //.setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
-      //.setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p><p><a href="' + feature.properties.URL + '">' + feature.properties.description + '</a></p>'>)
-      // .setHTML('<pre>'+JSON.stringify(feature.properties)+'</pre><h1>' + feature.properties.title + '</h1><p>' 
       .setHTML('<h1>' + feature.properties.title + '</h1><p>' 
       + feature.properties.description + '</p><p>' 
       + feature.properties.titleproductrange + '</p><p>'
