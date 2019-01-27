f2f48ac9438d82b6f8d8d5323d95d86b6a5afec2
ezoar <noquierotenermailperolotengo@gmail.com>
2019-01-27 21:47:20 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Token and Fixing Center-Point of Map 
 
-  // Token
+  
+  // Funktionierendes Beipsiel 1
 
   mapboxgl.accessToken = 'pk.eyJ1IjoiZW8tbWFwYm94IiwiYSI6ImNqcjEwYWN4ZzAzNDgzeXJzdGtlbGduNGgifQ.TBbP0PVssrORXh4sFvoOMA'; // replace this with your access token
   var map = new mapboxgl.Map({
@@ -10,11 +11,27 @@
     zoom: 10.7
   });
 
+  
+
+  /*
+  // Funktionierendes Beipsiel 2
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoiZW8tbWFwYm94IiwiYSI6ImNqcjEwYWN4ZzAzNDgzeXJzdGtlbGduNGgifQ.TBbP0PVssrORXh4sFvoOMA';
+  const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/eo-mapbox/cjr4zvhub1opj2rpbzrpu8fjf',
+  center: [12.397613, 51.315664],
+  zoom: 10.7
+  });
+  */
+
+
+
 // Markers and Popups
 
   map.on('click', function(e) {
   var features = map.queryRenderedFeatures(e.point, {
-      layers: ['leipzig-spätis']
+      layers: ['leipzig-spaetis'] // replace this with the name of the layer
   });
 
   if (!features.length) {
@@ -25,6 +42,9 @@
 
   var popup = new mapboxgl.Popup({ offset: [0, -15] })
       .setLngLat(feature.geometry.coordinates)
+      //.setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
+      //.setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p><p><a href="' + feature.properties.URL + '">' + feature.properties.description + '</a></p>'>)
+      // .setHTML('<pre>'+JSON.stringify(feature.properties)+'</pre><h1>' + feature.properties.title + '</h1><p>' 
       .setHTML('<h1>' + feature.properties.title + '</h1><p>' 
       + feature.properties.description + '</p><p>' 
       + feature.properties.titleproductrange + '</p><p>'
@@ -37,6 +57,8 @@
       
       .addTo(map);
   });
+
+
 
 // Directions
 
@@ -58,6 +80,7 @@
       },
       trackUserLocation: true
   }), 'top-right');
+
 
 // Service Worker Registration: Catch and pass to Service Worker file (sw.js)
 
